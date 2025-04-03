a0a18f1a3efeed494d76ad947089639889b2b5b6
RIN4D <167313776+rin4d@users.noreply.github.com>
2025-04-03 16:10:57 +0200

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,24 @@
+const translations = {
+    fr: {
+      title: "CHOISISSEZ VOTRE TÉLÉCOMMANDE :",
+      normal: "Télécommande classique",
+      hearing: "Télécommande auditive",
+      cognitive: "Télécommande simplifiée",
+      visual: "Télécommande visuelle adaptée",
+    },
+    en: {
+      title: "CHOOSE YOUR REMOTE CONTROL:",
+      normal: "Standard remote",
+      hearing: "Hearing accessible remote",
+      cognitive: "Simplified remote",
+      visual: "Visual accessible remote",
+    },
+    es: {
+      title: "ELIJA SU MANDO A DISTANCIA:",
+      normal: "Mando clásico",
+      hearing: "Mando auditivo",
+      cognitive: "Mando simplificado",
+      visual: "Mando visual adaptado",
+    }
+  };
+  
